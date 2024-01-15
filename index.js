5ea0d33fa18d9b9c6571bddc6aa05183b24fbe40
vinjal-jain <vinjaljain14it@gmail.com>
2024-01-15 18:24:32 +0530

    chaining the .then

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const readFilePro = file => {
     })
 }
 
-const writeFilePro = (file,data) => {
-    return new Promise ((resolve,reject) => {
-        fs.writeFile(file,data,err => {
-            if(err) reject('Could not write file');
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) reject('Could not write file');
             resolve('success');
         })
     })
@@ -23,20 +23,20 @@ const writeFilePro = (file,data) => {
 readFilePro(`${__dirname}/dog.txt`).then(data => {
     console.log(`Breed : ${data}`);
 
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
-            console.log(res.body.message);
-
-            fs.writeFile('dog-img.txt', res.body.message, err => {
-                if (err) return console.log(err.message);
-                console.log('Random dog image saved to file!');
-            });
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
 })
+    .then(res => {
+        console.log(res.body.message);
+
+        return writeFilePro('dog-img.txt', res.body.message)
+    })
+    .then(() => {
+        console.log('Random dog image saved to file!');
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
+
 
 
 
